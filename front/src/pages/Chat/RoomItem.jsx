2113e2f0c2e8cfa41aa12511941faa3c0d016464
ANHYUCK8150 <blackhyuck@gmail.com>
2022-08-28 21:50:41 +0900

    [FE]Feat: moment 추가

--- front/src/pages/Chat/RoomItem.jsx
+++ front/src/pages/Chat/RoomItem.jsx
@@ -1,14 +1,31 @@
 /* eslint-disable array-callback-return */
 import React from 'react';
+import Moment from 'react-moment';
 import { useNavigate } from 'react-router-dom';
 import style from './ChatPage.style';
 import noImg from '../../assets/img/logo/no_img.png';
+import 'moment/locale/ko';
 
 const RoomItem = ({ room, memberId }) => {
   const navigate = useNavigate();
 
   const onErrorImg = e => {
     e.target.src = noImg;
+  };
+
+  const displayCreatedAt = createdAt => {
+    let startTime = new Date(createdAt);
+    let nowTime = Date.now();
+    console.log(parseInt(startTime - nowTime));
+    if (parseInt(startTime - nowTime) > -60000) {
+      return <Moment format="방금 전">{startTime}</Moment>;
+    }
+    if (parseInt(startTime - nowTime) < -86400000) {
+      return <Moment format="YYYY년 MM월 DD일">{startTime}</Moment>;
+    }
+    if (parseInt(startTime - nowTime) > -86400000) {
+      return <Moment fromNow>{startTime}</Moment>;
+    }
   };
 
   const lastMessageDate = new Date(room.modifiedDate).toLocaleTimeString('ko-kr', { month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
@@ -23,7 +40,8 @@ const RoomItem = ({ room, memberId }) => {
       <InfoBox>
         <TitleBox>
           <p>{roomName}</p>
-          <span>{lastMessageDate}</span>
+          {/* <span>{lastMessageDate}</span> */}
+          <span>{displayCreatedAt(room.modifiedDate)}</span>
         </TitleBox>
         <SubBox>
           <p>{room.lastMessage}</p>
