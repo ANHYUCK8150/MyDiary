e5aaeb56a9323eab901a481e111ab444507c9a13
ANHYUCK8150 <blackhyuck@gmail.com>
2022-11-24 14:24:35 +0900

    Feat: admin 프로젝트 추가

---
+++ admin/src/main/java/com/diary/admin/MyDiaryAdminApplication.java
@@ -0,0 +1,13 @@
+package com.diary.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDiaryAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDiaryAdminApplication.class, args);
+	}
+
+}
