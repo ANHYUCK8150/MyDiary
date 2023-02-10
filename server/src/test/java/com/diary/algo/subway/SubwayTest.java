d35dc48851bcec1afe9d76e70470effb4886a4a9
ANHYUCK8150 <blackhyuck@gmail.com>
2023-02-10 20:17:22 +0900

    [Feat] Redis 추기

---
+++ server/src/test/java/com/diary/algo/subway/SubwayTest.java
@@ -0,0 +1,15 @@
+package com.diary.algo.subway;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SubwayTest {
+
+	@DisplayName("지하철 최소 환승 알고리즘")
+	@Test
+	void main() {
+
+	}
+}
